29d74691d4eaa9d886c8e2d88ea18f6d8f3f3b90
Noel Markham <noel.markham@gmail.com>
2015-02-22 18:13:59 +0000

    Working markov chain. Ish

--- src/main/scala/com/noelmarkham/lambdadays/external/Twitter.scala
+++ src/main/scala/com/noelmarkham/lambdadays/external/Twitter.scala
@@ -1,6 +1,9 @@
 package com.noelmarkham.lambdadays.external
 
+import scala.annotation.tailrec
 import scala.concurrent._
+import scala.concurrent.duration._
+import scala.util.Random
 
 import scalaz._
 import Scalaz._
@@ -16,13 +19,47 @@ object Twitter {
   implicit def detailsCodec: CodecJson[UserDetails] = casecodec1(UserDetails.apply, UserDetails.unapply)("screen_name")
   implicit def tweetCodec: CodecJson[Tweet] = casecodec3(Tweet.apply, Tweet.unapply)("user", "text", "created_at")
 
-  def getTweets(twitterHandle: String, apiKey: String, apiSecret: String): Future[List[Tweet]] = {
-    Future {
-      val source = scala.io.Source.fromFile("tweets.json").mkString
-      val tweets = Parse.decodeOption[List[Tweet]](source).getOrElse(throw new RuntimeException("Cannot parse response"))
-      tweets.filter {
-        case Tweet(UserDetails(screenName), _, _) => screenName === twitterHandle
-      }
+  def help(handle: String): String = Await.result(getTweets(handle, "", ""), 1.second).map(_.content).mkString(" ")
+
+  def getTweets(twitterHandle: String, apiKey: String, apiSecret: String): Future[List[Tweet]] = Future {
+    val source = scala.io.Source.fromFile(s"${twitterHandle.toLowerCase}.json").mkString
+    val tweets = Parse.decodeOption[List[Tweet]](source).getOrElse(throw new RuntimeException("Cannot parse response"))
+    tweets.filter {
+      case Tweet(UserDetails(screenName), content, _) => screenName.toLowerCase === twitterHandle.toLowerCase && !(content.startsWith("RT") || content.startsWith("@"))
     }
+  }
+}
+
+object Markov {
+
+  def generateString(text: String, context: Int): Future[String] = Future {
+    val tokens = text.split(" ").map(_.trim).filterNot(_ == "").toList
+
+    val parts = tokens.sliding(context).toList
+
+    val map: Map[List[String], List[String]] = parts.foldMap { words =>
+      Map(words.init -> List(words.last))
+    }
+
+    val (startingWords, _) = map.toList(Random.nextInt(map.size))
+
+    @tailrec
+    def go(key: List[String], acc: List[String]): List[String] = {
+
+      val nextWordList = map.get(key)
+
+      nextWordList match {
+        case Some(list) =>
+          val nextWord = list(Random.nextInt(list.size))
+          val nextKey = key.drop(1) ++ List(nextWord)
+          go(nextKey, nextWord :: acc)
+        case None => acc
+      }
+
+    }
+
+    val sentence = startingWords ++ go(startingWords, Nil).reverse
+
+    sentence.dropWhile(s => !s.charAt(0).isUpper).mkString(" ").split("(?<=[.?!] )").headOption.getOrElse("").trim
   }
 }
